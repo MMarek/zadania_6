daa9201a815f1c18497ee41c19cfec2fcde2e594
MMarek <mildnermarek@gmail.com>
2020-02-13 00:03:12 +0100

    add info to list

--- task11.js
+++ task11.js
@@ -2,6 +2,29 @@ console.log('EXERCISE 10:');
 
 document.addEventListener('DOMContentLoaded', function () {
 
-    const button = document
+    const button = document.querySelector('button');
+    console.log(button);
+    const input = document.querySelector('input');
+    console.log(input);
+    const table = document.querySelector('table');
+    console.log(table);
 
-})
+    button.addEventListener('click', function () {
+        console.log('click');
+        input[0].value;
+        input[1].value;
+        input[2].value;
+        console.log(input[0].value);
+
+        const tr = document.createElement('tr');
+        table.appendChild(tr);
+
+        let row = '';
+        for (let i = 0; i < input.length; i++) {
+            row += '<td>' + input[i].value + '</td>';
+            input[i].value = '';
+        }
+        tr.innerHTML = row;
+    });
+
+});
