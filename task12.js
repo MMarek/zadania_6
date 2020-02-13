b4e82bdac139314feafdb174a9cbbe960b2b5196
MMarek <mildnermarek@gmail.com>
2020-02-13 09:29:41 +0100

    add info to list correction

--- task12.js
+++ task12.js
@@ -1,10 +1,11 @@
 console.log('EXERCISE 12:');
 
 document.addEventListener('DOMContentLoaded', function f() {
-    const btn3 = document.querySelector('.remove');
+    const btn3 = document.querySelector('button');
     console.log(btn3);
 
-    btn3.addEventListener("click",function () {
+    btn3[11].addEventListener("click",function () {
         btn3.parentElement.removeChild(this);
+        console.log(btn3[11]);
     })
 });
