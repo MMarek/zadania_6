652da70005e5b805900793a8075c77c527ef33a2
MMarek <mildnermarek@gmail.com>
2020-02-13 14:24:04 +0100

    remove button

--- task12.js
+++ task12.js
@@ -1,10 +1,10 @@
 console.log('EXERCISE 12:');
 
 document.addEventListener('DOMContentLoaded', function f() {
-    const btn3 = document.querySelector('button');
+    const btn3 = document.querySelector('.remove');
     console.log(btn3);
 
-    btn3[11].addEventListener("click",function () {
+    btn3.addEventListener("click",function () {
         btn3.parentElement.removeChild(this);
         console.log(btn3[11]);
     })
